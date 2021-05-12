92be8b5904ed8eaf05d9ac5bd6716de2ac3104c7
prajwal sekhri <prajwalsekhri@gmail.com>
2021-05-12 19:39:51 +0530

    OK

---
+++ psms/psms-app/src/main/java/com/personal/psms/app/PSMSApplication.java
@@ -0,0 +1,16 @@
+package com.personal.psms.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class PSMSApplication extends SpringBootServletInitializer{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(PSMSApplication.class,args);
+
+	}
+
+}
