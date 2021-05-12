979ad454b2cc7487eb087d9da54229bf06b4a7e4
prajwal sekhri <prajwalsekhri@gmail.com>
2021-05-12 20:05:10 +0530

    added chnages

--- psms/psms-app/src/main/java/com/personal/psms/app/PSMSApplication.java
+++ psms/psms-app/src/main/java/com/personal/psms/app/PSMSApplication.java
@@ -1,10 +1,14 @@
 package com.personal.psms.app;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @SpringBootApplication
+@EnableAutoConfiguration
 public class PSMSApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
